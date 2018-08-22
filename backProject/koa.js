5637c46478c89d8b9d3bb94a786c6036c937c25d
金融检索 <luo73i50@163.com>
2018-08-22 16:42:24 +0800

    后台修改

--- backProject/koa.js
+++ backProject/koa.js
@@ -25,12 +25,34 @@ app.use(async ctx => {
     ctx.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     ctx.set('Access-Control-Allow-Headers', 'DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization');
 
+    var sql1 = 'SELECT id,title,url,net_name,date_format(ent_time,\'%Y-%m-%d\') as ent_time,keyword,digest,content,hot_degree from netfin_filtered_message as tt1 inner join (SELECT id as inid, count(1) as tc from netfin_keyword_search where id >? and DATE_FORMAT(ent_time,\'%Y-%m-%d\')>=? and DATE_FORMAT(ent_time,\'%Y-%m-%d\')<=?';
+    var paramList = [];
+    var sql2 = '';
+    var rst;
     try{
-      var rst = await mysqlQuery(connection, 'SELECT id,title,url,net_name,date_format(ent_time,\'%Y-%m-%d\') as ent_time,keyword,digest,content,hot_degree from netfin_source_message where id > ? and DATE_FORMAT(ent_time,\'%Y-%m-%d\')>=? and DATE_FORMAT(ent_time,\'%Y-%m-%d\')<=? and keyword like CONCAT(\'%\',?,\'%\') order by id asc limit 500;', [ctx.request.query.id, ctx.request.query.sday, ctx.request.query.eday, ctx.request.query.keyword])
+      if(ctx.request.query.fields=='2'){
+        sql1 += ' and ((n_type=\'企业\' and keyword=?) or (n_type=\'前沿技术\' and keyword=?)) GROUP BY id having tc=2) as tt2 where tt1.id = tt2.inid order by id asc limit 200;';
+        paramList = [ctx.request.query.id, ctx.request.query.sday, ctx.request.query.eday, ctx.request.query.key1,ctx.request.query.key2];
+        rst = await mysqlQuery(connection,sql1,paramList);
+
+      }else if(ctx.request.query.fields=='1'){
+        if(ctx.request.query.key1!=''){
+          sql1 += ' and n_type=\'企业\' and keyword=? GROUP BY id having tc=1) as tt2 where tt1.id = tt2.inid order by id asc limit 200;';
+          paramList = [ctx.request.query.id, ctx.request.query.sday, ctx.request.query.eday, ctx.request.query.key1];
+        }else if(ctx.request.query.key2!=''){
+          sql1 += ' and n_type=\'前沿技术\' and keyword=? GROUP BY id having tc=1) as tt2 where tt1.id = tt2.inid order by id asc limit 200;';
+          paramList = [ctx.request.query.id, ctx.request.query.sday, ctx.request.query.eday, ctx.request.query.key2];
+        }
+        rst = await mysqlQuery(connection,sql1,paramList)
+
+      }else if(ctx.request.query.fields=='0'){
+        sql2 = 'SELECT id,title,url,net_name,date_format(ent_time,\'%Y-%m-%d\') as ent_time,keyword,digest,content,hot_degree from netfin_filtered_message where id > ? and DATE_FORMAT(ent_time,\'%Y-%m-%d\')>=? and DATE_FORMAT(ent_time,\'%Y-%m-%d\')<=? order by id asc limit 200;'
+        paramList = [ctx.request.query.id, ctx.request.query.sday, ctx.request.query.eday];
+        rst = await mysqlQuery(connection, sql2, paramList);
+      }
     }catch(e){
       console.error(e)
     }
-    
     ctx.body = rst;
   });
   
